4875737aa36f6727e1c1c9eb87102f190f2db8d5
Sebastian Stauch <bugsteinpeter@gmail.com>
2019-02-15 12:08:24 +0100

    works.

--- cmd/yaml-renderer/main.go
+++ cmd/yaml-renderer/main.go
@@ -1,17 +1,73 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"strings"
+	"text/template"
+
+	"github.com/ghodss/yaml"
 )
 
 var (
-	buildtime    string
-	gitcommit    string
-	appversion   string
+	buildtime     string
+	gitcommit     string
+	appversion    string
+	templateFile  string
+	yamlFile      string
+	renderedBytes bytes.Buffer
 )
 
 func main() {
 	log.Printf("appversion: %s", appversion)
 	log.Printf("gitcommit:  %s", gitcommit)
 	log.Printf("buildtime:  %s", buildtime)
+
+	flag.StringVar(&templateFile, "t", "", "The path to the template file")
+	flag.StringVar(&yamlFile, "y", "", "The path to the yaml file with the data")
+
+	flag.Usage = func() {
+		fmt.Printf("%s - Render template with yaml input.\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	outputFileName := strings.Replace(templateFile, ".tpl", ".yaml", 1)
+
+	outputFile, err := os.OpenFile(outputFileName, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+
+	yamlFileContent, err := ioutil.ReadFile(yamlFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var yamlData map[string]interface{}
+	err = yaml.Unmarshal(yamlFileContent, &yamlData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	t, err := template.ParseFiles(templateFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = t.Execute(&renderedBytes, yamlData)
+	if err != nil {
+		fmt.Println("executing template:", err)
+		os.Exit(1)
+	}
+
+	_, err = outputFile.Write(renderedBytes.Bytes())
+	if err != nil {
+		panic(err)
+	}
+
 }
---
+++ test/file.tpl
@@ -0,0 +1,4 @@
+Hello world.
+
+this is {{ .data.name }}
+I can haz tags: {{ .data.taglist }}
